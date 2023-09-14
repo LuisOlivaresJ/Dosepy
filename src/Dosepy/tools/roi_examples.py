cd62bd6bb02912ca4a7ad5e673e8ea4799b023a2
Luis Alfonso Olivares Jiménez <alfonso.cucei.udg@gmail.com>
2023-09-13 20:12:29 -0700

    Update roi_examples.py
    RegionProperties

--- src/Dosepy/tools/roi_examples.py
+++ src/Dosepy/tools/roi_examples.py
@@ -1,7 +1,16 @@
 from pathlib import Path
 from image import load
 import matplotlib.pyplot as plt
-from tifffile import TiffFile
+from tifffile import TiffFile, imread
+
+from skimage.filters import threshold_otsu
+from skimage.color import rgb2gray
+from skimage.morphology import closing, square, erosion
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+
+import matplotlib.patches as mpatches
+
 
 demo_path = Path(__file__).parent.parent / "data" / "demo_calib.tif"
 
@@ -14,16 +23,63 @@ print(img.info)
 print("==================TIFF====================")
 # Get information about the image stack in the TIFF file without reading any image data:
 tif = TiffFile(demo_path)
+tImage = imread(demo_path)
 page = tif.pages[0]
 print(page.shape)
 print(page.dtype)
 print(page.tags['XResolution'].value[0])
 
 
-fig, axes = plt.subplots(ncols=3, figsize=(8, 2.5))
+fig, axes = plt.subplots(ncols=4, figsize=(8, 2.5))
 ax = axes.ravel()
-ax[0] = plt.subplot(1, 1, 1)
+ax[0] = plt.subplot(1, 4, 1)
+ax[1] = plt.subplot(1, 4, 2)
+ax[2] = plt.subplot(1, 4, 3)
+ax[3] = plt.subplot(1, 4, 4)
 
-ax[0].hist(img.array.ravel(), bins = 256)
+grayscale = rgb2gray(tImage)
+thresh = threshold_otsu(grayscale)
 
+# Apply threshold and close small holes with binary closing.
+# 9 = 75dpi * 3 mm, 9 is used to close smaller holes than 3 mm.
+
+#binary = grayscale > thresh
+binary = closing(grayscale < thresh, square(9)) 
+
+# remove artifacts connected to image border
+cleared = clear_border(binary)
+
+# label image regions
+label_image = label(cleared)
+label_image = erosion(label_image, square(24))
+
+ax[0].imshow(grayscale)
+#ax[1].hist(img.array.ravel(), bins = 256)
+ax[1].hist(grayscale.ravel(), bins = 20)
+ax[1].axvline(thresh, color='r')
+
+ax[2].imshow(binary, cmap=plt.cm.gray)
+ax[2].set_title('Binary')
+ax[2].axis('off')
+
+#plt.show()
+
+ax[3].imshow(tImage/(2**16))
+
+#regions = regionprops(label_image, tImage, offset = (50,50))
+regions = regionprops(label_image, tImage)
+for region in regions:
+    # take regions with large enough areas
+    if region.area >= 100:
+        # draw rectangle around segmented coins
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                  fill=False, edgecolor='red', linewidth=2)
+        ax[3].add_patch(rect)
+        print(region.intensity_mean[0])
+
+ax[3].set_axis_off()
+plt.tight_layout()
 plt.show()
+
+print(len(regions))
