d2a73a89ece1f3f4ba58e617d3ca164fd593e8cb
luis <alfonso.cucei.udg@gmail.com>
2024-02-15 12:00:23 -0700

    gui_v2

---
+++ src/Dosepy/tools/gui_v2.py
@@ -0,0 +1,36 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QVBoxLayout,
+    QWidget,
+    QLabel,
+    QTabWidget,
+)
+
+from gui_widgets.calibration import CalibrationWidget
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Film dosimetry")
+
+        tab_layout = QVBoxLayout()
+        self.setLayout(tab_layout)
+
+        tabs = QTabWidget()
+        tabs.addTab(CalibrationWidget(), "Calibration")
+        tabs.addTab(QLabel("In progress..."), "Film2Dose")
+        tabs.addTab(QLabel("In progress..."), "Analysis")
+        tab_layout.addWidget(tabs)
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+
+    root_window = MainWindow()
+    root_window.show()
+
+    app.exec()
+
---
+++ src/Dosepy/tools/gui_widgets/calibration.py
@@ -0,0 +1,56 @@
+"""
+A widget to manage film calibration.
+"""
+
+from PySide6.QtWidgets import (
+    QWidget,
+    QPushButton,
+    QHBoxLayout,
+    QComboBox,
+    QVBoxLayout,
+    QLabel,
+)
+from PySide6.QtCore import Qt
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qtagg import FigureCanvas
+
+class CalibrationWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Main layout used for two widgets: FigureCanvas and parameters_widget.
+        main_layout = QHBoxLayout()
+        self.setLayout(main_layout)
+        
+        fig = Figure(
+            #figsize=(3, 5),
+            layout="constrained"
+            )
+        self.axe_image = fig.add_subplot(2, 1, 1)
+        self.axe_curve = fig.add_subplot(2, 1, 2)
+        
+        main_layout.addWidget(FigureCanvas(fig), 1) 
+        """The second argument (1) is used as a strech factor. 
+        Widgets with higher stretch factors grow more on 
+        window resizing.
+        """
+        
+        parameters_widget = QWidget()
+        parameters_layout = QVBoxLayout()
+        parameters_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        parameters_widget.setLayout(parameters_layout)
+        self.open_button = QPushButton("Open")
+        self.channel_combo_box = QComboBox()
+        self.channel_combo_box.addItems(["Red", "Green", "Blue"])
+        self.fit_combo_box = QComboBox()
+        self.fit_combo_box.addItems(["Rational", "Polynomial"])
+        parameters_layout.addWidget(self.open_button)
+        parameters_layout.addSpacing(30)
+        parameters_layout.addWidget(QLabel("Channel:"))
+        parameters_layout.addWidget(self.channel_combo_box)
+        parameters_layout.addWidget(QLabel("Fit function:"))
+        parameters_layout.addWidget(self.fit_combo_box)
+        
+        main_layout.addWidget(parameters_widget)
+        print(self.children())
