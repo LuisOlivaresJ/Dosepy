7205b7cf7ef7ffd77a667186cca95c48b396e0a6
luis <alfonso.cucei.udg@gmail.com>
2024-09-08 11:13:15 -0700

    use of sh file for new version automation

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # How to use this script:
-# 1. Run this script from the root directory of the project as ./_build_and_install.sh
+# 1. Run this script from the root directory of the project as ./build_and_install.sh
 # 2. The script will create a source distribution of the package and install it.
 # 3. The script will also uninstall the package if it is already installed.
 
@@ -13,8 +13,8 @@ rm -rf dist
 python3 -m pip uninstall -y Dosepy
 
 # This script is used to package the application for distribution.
-python3 -m pip install --upgrade pip
-pyphon3 -m pip install --upgrade build
+#python3 -m pip install --upgrade pip
+#pyphon3 -m pip install --upgrade build
 #python3 -m pip install --upgrade twine
 
 # Create a source distribution
