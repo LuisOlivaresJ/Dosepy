465643bbb28b1ad421c3ce60267b2d8a8eca5341
Luis Olivares <alfonso.cucei.udg@gmail.com>
2025-04-24 07:29:04 -0700

    chi documentation

--- src/Dosepy/tools/gamma.py
+++ src/Dosepy/tools/gamma.py
@@ -11,38 +11,7 @@ def is_close(val: float, target: float, delta: float):
     if target - delta < val < target + delta:
         return True
     return False
-
-
-class GammaManager:
     
-    def gamma(
-        method,
-        reference: ArrayImage,
-        evaluated: ArrayImage,
-        params: dict,
-     ):
-        """
-        Parameters
-        ----------
-
-        method : str
-            ExhaustiveSearch: Compute gamma for every pixel.
-            Chi: Computes comparision based on `Bakai et al <http://iopscience.iop.org/0031-9155/48/21/006/>`_ eq.6,
-        params: {dose_ta: value, dist_ta: value, }
-        """
-        # TODO
-        if method == "ExhaustiveSearch":
-            return evaluated.gamma2D(
-                 dose_ta=3,
-                 dist_ta=3
-            )[0]
-        
-        if method == "FastGamma_Chi":
-             return chi(
-                  reference_image=reference,
-                  comparison_image=evaluated,
-             )
-        
 
 def chi(
         reference_image: ArrayImage,
@@ -52,10 +21,10 @@ def chi(
         threshold: float = 10,
         interpolate: bool = True,
     ) -> np.ndarray:
-        """Calculate the gamma between the current image (reference) and a comparison image.
-        Adapted from pylinac.
+        """Calculate the chi between the current image (reference) and a comparison image.
+        Adapted from pylinac.core.image.
 
-        The gamma calculation is based on `Bakai et al
+        The chi calculation is based on `Bakai et al
         <http://iopscience.iop.org/0031-9155/48/21/006/>`_ eq.6,
         which is a quicker alternative to the standard Low gamma equation.
 
@@ -73,16 +42,14 @@ def chi(
         threshold : float
             The dose threshold percentage of the maximum dose, below which is not analyzed.
             Must be between 0 and 100.
-        interpolate : bool
-            If True the arrays are zoomed using spline interpolation to have a spatial resolution of dpmm = 1.
 
         Returns
         -------
-        gamma_map : numpy.ndarray
-            The calculated gamma map.
+        gamma_map, chi_rate : numpy.ndarray, float
+            The calculated chi map and passing rate.
 
         """
-        # error checking
+        # Error checking
         if not is_close(reference_image.dpmm, comparison_image.dpmm, delta=1):
             raise AttributeError(
                 f"The image resolution do not match: {reference_image.dpi:.2f} vs. {comparison_image.dpi:.2f}"
@@ -94,8 +61,8 @@ def chi(
                 f"The images are not the same size: {reference_image.shape} vs. {comparison_image.shape}"
             )
 
-        # interpolate reference and comparison images to have a 
-        # spatial resolution of dpmm = 2 (dpi = 50.8)
+        # Interpolate reference and comparison images to dpmm = 2 (dpi = 50.8).
+        # This is 1/4 of 2 mm (a commonly used tolerance limit) accordingly to AAPM TG218 recomendations,
         new_dpmm = 2
         if interpolate:
             ref_img = ArrayImage(
@@ -108,18 +75,18 @@ def chi(
                 dpi=new_dpmm*MM_PER_INCH,
             )
 
-        # convert distance value from mm to pixels
+        # Convert distance value from mm to pixels
         distTA_pixels = reference_image.dpmm * distTA
 
         # Calculate doseTa
         doseTA_Gray = doseTA/100 * np.amax(ref_img.array)
 
-        # construct image gradient using sobel filter
+        # Construct image gradient
         img_x = np.gradient(ref_img.array, axis=1)
         img_y = np.gradient(ref_img.array, axis=0)
         grad_img = np.hypot(img_x, img_y)
 
-        # equation: (measurement - reference) / sqrt ( doseTA^2 + distTA^2 * image_gradient^2 )
+        # Equation: (measurement - reference) / sqrt ( doseTA^2 + distTA^2 * image_gradient^2 )
         subtracted_img = np.abs(comp_img.array - ref_img.array)
         denominator = np.sqrt(
             ((doseTA_Gray) ** 2) + ((distTA_pixels**2) * (grad_img**2))
@@ -127,7 +94,7 @@ def chi(
 
         chi_map = subtracted_img / denominator
 
-        # invalidate dose values below threshold
+        # Invalidate dose values below threshold
         chi_map[ref_img.array < threshold/100 * np.max(ref_img.array)] = np.nan
 
         # Number of values that are not np.nan
